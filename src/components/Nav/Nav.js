df6ca2ef3aa19f3374f4264f9a44fcac4283fb29
James Posey <jamesrposeyjr@gmail.com>
2021-01-27 08:36:25 -0600

    adjusted nav par for mongo

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -11,34 +11,34 @@ const Nav = (props) => {
     text: 'Login / Register',
   };
 
-  if (props.store.user.id != null) {
+  if (props.store.user._id != null) {
     loginLinkData.path = '/user';
     loginLinkData.text = 'Home';
   }
 
   return (
-    <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
+    <div className='nav'>
+      <Link to='/home'>
+        <h2 className='nav-title'>Prime Solo Project</h2>
       </Link>
-      <div className="nav-right">
-        <Link className="nav-link" to={loginLinkData.path}>
+      <div className='nav-right'>
+        <Link className='nav-link' to={loginLinkData.path}>
           {/* Show this link if they are logged in or not,
           but call this link 'Home' if they are logged in,
           and call this link 'Login / Register' if they are not */}
           {loginLinkData.text}
         </Link>
         {/* Show the link to the info page and the logout button if the user is logged in */}
-        {props.store.user.id && (
+        {props.store.user._id && (
           <>
-            <Link className="nav-link" to="/info">
+            <Link className='nav-link' to='/info'>
               Info Page
             </Link>
-            <LogOutButton className="nav-link" />
+            <LogOutButton className='nav-link' />
           </>
         )}
         {/* Always show this link since the about page is not protected */}
-        <Link className="nav-link" to="/about">
+        <Link className='nav-link' to='/about'>
           About
         </Link>
       </div>
