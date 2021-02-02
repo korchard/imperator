87f8e89d96d7f3c14e2ecfedd2d9a12acc85cf63
James Posey <jamesrposeyjr@gmail.com>
2021-02-02 17:36:51 -0600

    bar renamed

--- src/components/Operational/Operational.tsx
+++ src/components/Operational/Operational.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 // import ChartsEmbedSDK from '@mongodb-js/charts-embed-dom';
-import Bar from './Bar';
+import BarGraph from './BarGraph';
 
 const Operational: React.FC = () => {
   return (
     <div>
       <h1>Operational</h1>
       <script src='https://charts.mongodb.com/charts-project-0-teizd'></script>
-      <Bar />
+      <BarGraph />
     </div>
   );
 };
