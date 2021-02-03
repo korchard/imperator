45392d0ee4c18ef9dc241735a3abff35f0918fdb
Josie Fredericksen <josiefredericksen@Josies-MacBook-Pro.local>
2021-02-03 09:22:41 -0600

    converted footer to ts

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+// export default Footer;
+const Footer: React.FC = () => {
+    return (
+      <div>
+          <footer>&copy; Aurelius</footer>
+      </div>
+    );
+};
+
+export default Footer;
+  
