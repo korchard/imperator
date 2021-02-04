fbaf0d5e2ac8d51de4bd939c04d005d2e78f38cd
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2021-02-04 11:00:36 -0600

    working on the aggregation for the table in the controller

--- server/controllers/imperator.ts
+++ server/controllers/imperator.ts
@@ -6,7 +6,7 @@ const imperator = async (
     res: Response
   ): Promise<void> => {
     console.log('getting data for the table');
-    try {
+    try { // this needs to be an AGGREGATION, not a FIND
         const data = await CompanyDB.find({}, 
                 {
                     "company": 1, 
@@ -15,7 +15,68 @@ const imperator = async (
                     "jira": { $or: true },
                     "zapier": { $or: true },
                 }
-             )
+             );
+
+             // currently not functioning  
+             await CompanyDB.aggregate([
+             {$count: 'Total_Companies'}, 
+                 {
+                     $lookup: {
+                         from: "hashtags",
+                         pipeline: [{
+                             $group: {
+                                 '_id': 0,
+                             'count': {$sum: 1}
+                             }
+                             }],
+                         as: "hashtags"
+                     },
+                 }, {
+                     $lookup: {
+                         from: "insights",
+                         pipeline: [{
+                             $group: {
+                                 '_id': 0,
+                             'count': {$sum: 1}
+                             }
+                             }],
+                         as: "insights"
+                     },
+                 }, {
+                     $lookup: {
+                         from: "notes",
+                         pipeline: [{
+                             $group: {
+                                 '_id': 0,
+                             'count': {$sum: 1}
+                             }
+                             }],
+                         as: "notes"
+                     },
+                 }, {
+                     $lookup: {
+                         from: "projects",
+                         pipeline: [{
+                             $group: {
+                                 '_id': 0,
+                             'count': {$sum: 1}
+                             }
+                             }],
+                         as: "projects"
+                     }
+                 },
+                 {$unwind: {path: "$hashtags"}},
+                 {$unwind: {path: "$insights"}},
+                 {$unwind: {path: "$notes"}},
+                 {$unwind: {path: "$projects"}},
+                 {$project: {
+                     'totalNotes': {$sum: ["$notes.count"]},
+                     'totalHashtags': {$sum: ["$hashtags.count"]},
+                     'totalInsights': {$sum: ["$insights.count"]},
+                     'totalProjects': {$sum: ["$projects.count"]},
+                     }
+                 }
+             ]);
              res.send(data)
              console.log('imperator controller', data)
     } catch (error) {
