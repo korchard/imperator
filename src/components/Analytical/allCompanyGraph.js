25d66167ea90823c5cb0910f59c8017bb6f5ad80
Malik Elate <Malik.e1955@gmail.com>
2021-02-05 13:13:07 -0600

    removed options

--- src/components/Analytical/allCompanyGraph.js
+++ src/components/Analytical/allCompanyGraph.js
@@ -57,63 +57,8 @@ const SingleCompanyGraph = () => {
   )
   const location = useRouteMatch();
   useEffect(() => {
-    checkSingleOrAll()
+    dispatch({ type: 'FETCH_TOTAL_ACTIONS' });
   }, []);
-  
-  const checkSingleOrAll = () => { 
-    if (location.params.type === 'single')   { 
-      dispatch({ type: 'GET_USERS_BY_MONTH' });
-      setOpt({
-        series: [{
-          name: 'This company',
-          data: [56, 61, 58, 63, 60, 66]
-        }, {
-          name: 'Average company',
-          data: [98, 87, 15, 91, 14, 94]
-        }],
-        options: {
-          chart: {
-            type: 'bar',
-            height: 350
-          },
-          plotOptions: {
-            bar: {
-              horizontal: false,
-              columnWidth: '55%',
-              endingShape: 'rounded'
-            },
-          },
-          dataLabels: {
-            enabled: false
-          },
-          stroke: {
-            show: true,
-            width: 2,
-            colors: ['transparent']
-          },
-          xaxis: {
-            categories: ['Hashtags', 'Documents', 'Insights', 'Notes', 'Projects', "Total"]
-          },
-          yaxis: {
-            title: {
-              text: 'Actions'
-            }
-          },
-          fill: {
-            opacity: 1
-          },
-          tooltip: {
-            y: {
-              formatter: function () {
-                return "$thousands"
-              }
-            }
-          }
-        },
-      })
-    }
-  }
-
 
   return (
     <div>
