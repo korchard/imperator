781235e9cc987dd45420a9f00f2cb6a08fd65f98
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2021-02-05 10:01:54 -0600

    completed backend testing for monthlyusers over time graph

---
+++ src/redux/reducers/monthlyusers.reducer.test.js
@@ -0,0 +1,15 @@
+import monthlyUsersOverTime from './monthlyusers.reducer';
+
+describe('Testing the monthlyusersReducer', () => {
+  test('test returns default state if it is not an action we assigned', () => {
+    let previousState = { something: 'foo' };
+    let action = { type: 'BAR' };
+    expect(userReducer(previousState, action)).toBe(previousState);
+  });
+
+  test('test returns the action.payload if we SET_MONTHLY_USERS', () => {
+    let previousState = { something: 'foo' };
+    let action = { type: 'SET_MONTHLY_USERS', payload: [12] };
+    expect(userReducer(previousState, action)).toEqual([12] );
+  });
+});
