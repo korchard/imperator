5650a650def69053f8a661b23b4203d180fb5993
Malik Elate <Malik.e1955@gmail.com>
2021-02-08 13:41:30 -0600

    added a allcompany controller

--- server/controllers/companyAnalytics.ts
+++ server/controllers/companyAnalytics.ts
@@ -27,7 +27,7 @@ export const singleCompany = async (
         $project: {
           "_id": 0, 
           "email": 1, 
-          company: 1, 
+          "company": 1, 
           "billing.status": 1,
           "billing.customerId": 1,
           "billing.plan": 1,
@@ -45,6 +45,107 @@ export const singleCompany = async (
         }},
     ]);
     res.send(data[0]);
+  } catch (error) {
+    console.error('Error getting total actions: ', error);
+  }
+};
+
+export const allCompany = async ( req: Request, res: Response ): Promise<void> => {
+  try {
+    const data = await CompanyDB.aggregate([
+      { $count: 'Total_Companies' },
+      {
+        $lookup: {
+          from: 'hashtags',
+          pipeline: [
+            {
+              $group: {
+                _id: 0,
+                count: { $sum: 1 },
+              },
+            },
+          ],
+          as: 'hashtags',
+        },
+      },
+      {
+        $lookup: {
+          from: 'documents',
+          pipeline: [
+            {
+              $group: {
+                _id: 0,
+                count: { $sum: 1 },
+              },
+            },
+          ],
+          as: 'documents',
+        },
+      },
+      {
+        $lookup: {
+          from: 'insights',
+          pipeline: [
+            {
+              $group: {
+                _id: 0,
+                count: { $sum: 1 },
+              },
+            },
+          ],
+          as: 'insights',
+        },
+      },
+      {
+        $lookup: {
+          from: 'notes',
+          pipeline: [
+            {
+              $group: {
+                _id: 0,
+                count: { $sum: 1 },
+              },
+            },
+          ],
+          as: 'notes',
+        },
+      },
+      {
+        $lookup: {
+          from: 'projects',
+          pipeline: [
+            {
+              $group: {
+                _id: 0,
+                count: { $sum: 1 },
+              },
+            },
+          ],
+          as: 'projects',
+        },
+      },
+      { $unwind: { path: '$hashtags' } },
+      { $unwind: { path: '$documents' } },
+      { $unwind: { path: '$insights' } },
+      { $unwind: { path: '$notes' } },
+      { $unwind: { path: '$projects' } },
+      {
+        $project: {
+          'hashtags._id': 0,
+          'documents._id': 0,
+          'insights._id': 0,
+          'notes._id': 0,
+          'projects._id': 0,
+        },
+      },
+    ]);
+    res.send([
+      (data[0].hashtags.count)/(data[0].Total_Companies),
+      (data[0].documents.count)/(data[0].Total_Companies),
+      (data[0].insights.count)/(data[0].Total_Companies),
+      (data[0].notes.count)/(data[0].Total_Companies),
+    ]);
+    console.log(data[0]);
   } catch (error) {
     console.error('Error getting total actions: ', error);
   }
--- server/routes/company.analytics.router.ts
+++ server/routes/company.analytics.router.ts
@@ -3,9 +3,12 @@ import { Request, Response } from 'express';
 import express from 'express';
 import rejectUnauthenticated from '../modules/authentication-middleware';
 import { singleCompany } from '../controllers/companyAnalytics'; 
+import { allCompany } from '../controllers/companyAnalytics'; 
 
 const router: express.Router = express.Router();
 
 router.get('/:id', singleCompany);
+
+router.get('/all/company', allCompany);
 
 export default router;
