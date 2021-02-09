830f7a0e76dfccb801009fa4610360581d2bd716
freder48 <jfredericksen12@gmail.com>
2021-02-09 16:00:42 -0600

    paid plans/trial plans expiring this month rendering to dom

--- src/components/Strategic/Strategic.js
+++ src/components/Strategic/Strategic.js
@@ -7,6 +7,8 @@ import './Strategic.css';
 function Strategic() {
   const dispatch = useDispatch();
   const strategic = useSelector((redux) => redux.strategic.strategicGraph.strategicGraph);
+  const paidPlan = useSelector((redux) => redux.strategic.paidPlans);
+  const trialPlan = useSelector((redux) => redux.strategic.trialPlans);
 
   useEffect(() => {
     dispatch({ type: 'GET_PLAN_LENGTH_AVERAGE' });
@@ -22,10 +24,22 @@ function Strategic() {
       <div className="listContainer">
         <div className="paidContainer">
           <h4>Paid Plans Ending This Month</h4>
+          {paidPlan.map(plan => {
+            return (
+              <p>{plan.company}</p>
+            )
+          }
+          )}
         </div>
 
         <div className="trialContainer">
           <h4>Trials Ending This Month</h4>
+          {trialPlan.map(plan => {
+            return (
+              <p>{plan.company}</p>
+            )
+          }
+          )}
         </div>
 
       </div>
