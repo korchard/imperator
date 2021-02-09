7e8a41f8bf090913ae2932429a937871c8450d4a
freder48 <jfredericksen12@gmail.com>
2021-02-09 09:42:35 -0600

    commented out chart

--- src/components/Strategic/StrategicGraph.js
+++ src/components/Strategic/StrategicGraph.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import Chart from 'react-apexcharts';
+// import Chart from 'react-apexcharts';
 
 function StrategicGraph() {
     const dispatch = useDispatch();
