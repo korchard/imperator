4717c9fd18fc4d70ca4af6ed9fbe11016e3ce274
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2021-02-09 15:57:38 -0600

    created a reducer for the single companies users

--- src/redux/reducers/singleCompany.reducer.js
+++ src/redux/reducers/singleCompany.reducer.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 // store imperator data for the table
 const singleCompanyData = (state = {}, action) => {
     switch (action.type) {
@@ -7,5 +9,19 @@ const singleCompanyData = (state = {}, action) => {
         return state;
     }
 };
+
+const singleCompanyUsers = (state = [], action) => {
+  switch (action.type) {
+    case 'SET_SINGLE_COMPANY_USERS':
+      return action.payload;
+    default:
+      return state;
+  }
+};
   
+const singleCompanyData = combineReducers({
+  singleCompanyData,
+  singleCompanyUsers,
+});
+
 export default singleCompanyData;
