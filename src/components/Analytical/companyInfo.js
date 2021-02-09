72e843aab5efb306bdfdda7e64f1acf7b2ec053b
Brady Baker <bradybaker82@gmail.com>
2021-02-09 10:01:48 -0600

    Conditional config information is displaying

--- src/components/Analytical/companyInfo.js
+++ src/components/Analytical/companyInfo.js
@@ -1,19 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useRouteMatch } from 'react-router-dom';
+import React from 'react';
+import { useSelector} from 'react-redux';
 import Moment from 'react-moment'
 
 const CompanyInfo = () => {
-  const location = useRouteMatch();
   const companyInfo = useSelector((store) => store.singleCompanyData);
-  const dispatch = useDispatch();
 
-  // let subStart = new Date(34535645 * 1000);
-  // console.log('Sub start', subStart)
-  
-  
-
-console.log('CompanyInfo', companyInfo.activeUntil)
+console.log('CompanyInfo', companyInfo
+)
   return (
     <div>
         <div className='companyInfoHeader'>
@@ -29,6 +22,22 @@ console.log('CompanyInfo', companyInfo.activeUntil)
           <div>
             Active Until: <Moment format='MM/DD/YYYY'>{companyInfo.activeUntil}</Moment>
           </div>
+          {companyInfo.jira && 
+          <div className='configInfo'>
+           <div>Jira Information:</div> 
+           <div>Token: {companyInfo.jira.api_token}</div> 
+           <div>Domain: {companyInfo.jira.domain} </div>
+           <div>Email: {companyInfo.jira.email}</div>
+        </div>
+          }
+          {companyInfo.zapier && 
+          <div className='configInfo'>
+           Zapier Information: 
+           Token: {companyInfo.zapier.api_token} 
+           Domain: {companyInfo.zapier.domain} 
+           Email: {companyInfo.zapier.email}
+        </div>
+          }
         </div>
     </div>
   );
