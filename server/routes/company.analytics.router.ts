fcfa4e7cf7ad5c20ef7944db59afae60af89606c
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2021-02-09 16:07:54 -0600

    created a route for single company users and a controller to grab the data from the database

--- server/routes/company.analytics.router.ts
+++ server/routes/company.analytics.router.ts
@@ -3,9 +3,12 @@ import { Request, Response } from 'express';
 import express from 'express';
 import rejectUnauthenticated from '../modules/authentication-middleware';
 import { singleCompany } from '../controllers/companyAnalytics'; 
+import { singleCompanyUsers } from '../controllers/companyAnalyticsUsers'; 
 
 const router: express.Router = express.Router();
 
 router.get('/:id', rejectUnauthenticated, singleCompany);
+
+router.get('/:id', rejectUnauthenticated, singleCompanyUsers);
 
 export default router;
