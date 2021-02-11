048889508b7e83d39e5c6a9c90cb22165d531657
freder48 <jfredericksen12@gmail.com>
2021-02-11 14:31:35 -0600

    modified trial plans expiring this month

--- server/controllers/strategicTrialPlan.controller.ts
+++ server/controllers/strategicTrialPlan.controller.ts
@@ -5,24 +5,31 @@ const strategicTrialPlan = async (req: Request, res: Response): Promise<void> =>
     try {
       const data = await CompanyDB.aggregate(
         [
+          {$match: {
+            'billing.plan': 'Trial'
+          }},
           {$project: {
             _id: 0,
+            
             company: 1,
             month: {$month: {$toDate: {$multiply: ["$billing.trialEnd", 1000]}}},
             year: {$year: {$toDate: {$multiply: ["$billing.trialEnd", 1000]}}}
           }},
           {$match: {
+          
           "month":new Date().getMonth() +1,
           "year": new Date().getFullYear()
           }
         }
-        ]
+      ]
      )
      res.send(data)
     } catch (error) {
       console.error('Error in strategicTrialPlan controller', error);
     }
 };
+
+
 
   
 export default strategicTrialPlan;
