a9903c66c57da597da62bf6b31e21a8e178d718e
James Posey <jamesrposeyjr@gmail.com>
2021-02-12 10:04:47 -0600

    fixed grid too

--- src/components/Imperator/Imperator.js
+++ src/components/Imperator/Imperator.js
@@ -25,41 +25,41 @@ const Imperator = () => {
 
   return (
     <div className='flexbox2'>
-      <div>
+      <div className='gridbox2'>
         <div className='headerArea'>
           <h1>Imperator</h1>
         </div>
         <div className='imperator-card'>
-        <div className='search-imperator'>
-          <input
-            className='search-input'
-            placeholder='Search Company Name'
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <button
-            className='btnI'
-            type='submit'
-            name='submit'
-            value='Clear'
-            onClick={clearSearch}
-          >
-            Clear
-          </button>
-          <button
-            className='btnI'
-            type='submit'
-            name='submit'
-            value='Find'
-            onClick={searchCo}
-          >
-            Find
-          </button>
+          <div className='search-imperator'>
+            <input
+              className='search-input'
+              placeholder='Search Company Name'
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <button
+              className='btnI'
+              type='submit'
+              name='submit'
+              value='Clear'
+              onClick={clearSearch}
+            >
+              Clear
+            </button>
+            <button
+              className='btnI'
+              type='submit'
+              name='submit'
+              value='Find'
+              onClick={searchCo}
+            >
+              Find
+            </button>
+          </div>
+          <div className='tableGrid'>
+            <Table data={imperator} pageCount={pageCount} />
+          </div>
         </div>
-        <div className='tableGrid'>
-          <Table data={imperator} pageCount={pageCount} />
-        </div>
-      </div>
       </div>
     </div>
   );
