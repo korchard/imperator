2f6af4c95be4d114fe98d727fea7be0bc6dd312e
Malik Elate <Malik.e1955@gmail.com>
2021-02-15 11:02:42 -0600

    fixed average actions graph dummy data

--- src/components/Analytical/singleCompanyGraph.js
+++ src/components/Analytical/singleCompanyGraph.js
@@ -113,7 +113,7 @@ const SingleCompanyGraph = () => {
         series={[
           {
             name: 'Average Aurelius Actions',
-            data: [12, 12, 10, 20, 30, 84],
+            data: [22, 22, 20, 40, 60, 168],
           },
           {
             name: `${singleCompanyData.company} Actions`,
